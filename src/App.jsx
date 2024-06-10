8407e6483a77f02e2de16cc6922300cd5314fd6b
Zach Hamilton <zachmhamilton@outlook.com>
2024-06-10 17:23:36 -0400

    component setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Home, Experience, Portfolio, Contact } from './components';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#194371',
+    },
+    secondary: {
+      main: '#060f19',
+    },
+    accent: {
+      main: '#427fbf',
+    },
+    text: {
+      main: '#f7fafd',
+    },
+    background: {
+      main: '#010204',
+    },
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <ThemeProvider theme={theme}>
+        <Home />
+        <Experience />
+        <Portfolio />
+        <Contact />
+      </ThemeProvider>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Contact from './Contact';
+import Experience from './Experience';
+import Portfolio from './Portfolio';
+import Home from './Home';
+
+export { Contact, Experience, Portfolio, Home };
