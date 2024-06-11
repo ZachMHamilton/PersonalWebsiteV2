2f452f08507e2db5dba21b191d82dfc536f8c6a4
Zach Hamilton <zachmhamilton@outlook.com>
2024-06-11 00:05:41 -0400

    created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { Home, Experience, Portfolio, Contact } from './components';
+import { Home, Experience, Portfolio, Contact, Nav } from './components';
 
 function App() {
   return (
     <div className="flex flex-col gap-2">
+      <Nav />
       <Home />
       <Experience />
       <Portfolio />
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Contact from './Contact';
 import Experience from './Experience';
 import Portfolio from './Portfolio';
 import Home from './Home';
+import Nav from './Nav';
 
-export { Contact, Experience, Portfolio, Home };
+export { Contact, Experience, Portfolio, Home, Nav };
