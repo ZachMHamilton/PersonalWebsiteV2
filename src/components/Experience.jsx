58908db7a2d05d1171a54ceae3687dbcef44ee03
Zach Hamilton <zachmhamilton@outlook.com>
2024-06-11 14:26:21 -0400

    framer motion for experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,140 +1,109 @@
 import { useState } from 'react';
-
-
-const workExperiences = [
-  {
-    title: 'Software Development Specialist',
-    company: 'Ohio Department of Taxation',
-    period: "July '24 - Present",
-    details: [
-      'Tax stuff tax stuff tax stuff tax stuff tax stuff tax stuff',
-      'Tax stuff tax stuff tax stuff tax stuff tax stuff tax stuff',
-    ],
-  },
-  {
-    title: 'Software Engineer',
-    company: 'Linkt',
-    period: "Jan. '24 - June '24",
-    details: [
-      'Wrote some awesome code and did awesome things so awesomly',
-      'Wrote some awesome code and did awesome things so awesomly',
-      'Wrote some awesome code and did awesome things so awesomly',
-    ],
-  },
-  {
-    title: 'Co-Creator & Software Engineer',
-    company: 'Lambda Peeler (Open Source)',
-    period: "July '23 - Jan. '24",
-    details: [
-      'LAMBDA LAMBDA LAMBDA LAMBDA LAMBDA LAMBDA LAMBDA',
-      'LAMBDA  LAMBDA LAMBDA LAMBDA LAMBDA LAMBDA',
-      'LAMBDA LAMBDA LAMBDA LAMBDA LAMBDA LAMBDA LAMBDA',
-    ],
-  },
-  {
-    title: 'Java Teaching Assistant',
-    company: 'The Ohio State University',
-    period: "Aug. '22 - Dec. '22",
-    details: [
-      'Go bucks! Go bucks! Go bucks! Go bucks! Go bucks! Go bucks!',
-      'Go bucks! Go bucks! Go bucks! Go bucks! Go bucks! Go bucks!',
-      'Go bucks! Go bucks! Go bucks! Go bucks! Go bucks! Go bucks!',
-    ],
-  },
-];
+import experienceData from '../data/experienceData';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../utils/motion';
+import { useFadeInOnInView } from '../utils/useFadeInOnView';
 
 const Experience = () => {
+  const { ref: headerRef, controls: headerControls } = useFadeInOnInView(0.1);
   const [selectedExperience, setSelectedExperience] = useState(
-    workExperiences[0]
+    experienceData[0]
   );
 
   return (
-    <div
-      id="experience"
-      className="flex flex-col items-center gap-8 xtra:gap-16 p-8 md:p-12 lg:p-24"
+    <motion.div
+      ref={headerRef}
+      animate={headerControls}
+      initial="hidden"
+      variants={fadeIn('up', 'spring', 0, 2)}
     >
-      <div className="text-text text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
-        Experience
-      </div>
-      <div className="flex flex-col items-start justify-start gap-12 hidden xtra:block w-full">
-        <div className="flex items-end">
-          <span className="text-accent text-lg sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
-            Languages&nbsp;
-          </span>
-          <p className="text-white text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl">
-            &nbsp; JavaScript | TypeScript | Java | SQL
-          </p>
+      <div
+        id="experience"
+        className="flex flex-col items-center gap-8 xtra:gap-16 p-8 md:p-12 lg:p-24"
+      >
+        <div className="text-text text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
+          Experience
         </div>
-        <div className="flex items-end">
-          <span className="text-accent text-lg sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
-            Technologies&nbsp;
-          </span>
-          <p className="text-white text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl">
-            &nbsp; React | Node | Express | AWS SDK | PostgreSQL | MongoDB |
-            Redux | Git | Jest | HTML | CSS | JWT | Heroku
-          </p>
+        <div className="flex flex-col items-start justify-start gap-12 hidden xtra:block w-full">
+          <div className="flex items-end">
+            <span className="text-accent text-lg sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
+              Languages&nbsp;
+            </span>
+            <p className="text-white text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl">
+              &nbsp; JavaScript | TypeScript | Java | SQL
+            </p>
+          </div>
+          <div className="flex items-end">
+            <span className="text-accent text-lg sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
+              Technologies&nbsp;
+            </span>
+            <p className="text-white text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl">
+              &nbsp; React | Node | Express | AWS SDK | PostgreSQL | MongoDB |
+              Redux | Git | Jest | HTML | CSS | JWT | Heroku
+            </p>
+          </div>
         </div>
-      </div>
-      {/* Experience section starts here */}
-      <div className="flex flex-col items-center justify-center gap-24 w-full h-full">
-        <div className="flex flex-col lg:flex-row items-start justify-between w-full gap-4 lg:gap-16">
-          <div className="flex flex-col gap-8 w-full lg:w-2/5">
-            {workExperiences.map((experience, index) => (
-              <div
-                key={index}
-                className={'cursor-pointer rounded-md'}
-                onClick={() => setSelectedExperience(experience)}
-              >
-                <h3
-                  className={`text-lg sm:text-xl md:text-2xl ${
-                    selectedExperience === experience
-                      ? 'text-accent'
-                      : 'text-gray-700'
-                  }`}
+        {/* Experience section starts here */}
+        <div className="flex flex-col items-center justify-center gap-24 w-full h-full">
+          <div className="flex flex-col lg:flex-row items-start justify-between w-full gap-4 lg:gap-16">
+            <div className="flex flex-col gap-8 w-full lg:w-2/5">
+              {experienceData.map((experience, index) => (
+                <div
+                  key={index}
+                  className={'cursor-pointer rounded-md'}
+                  onClick={() => setSelectedExperience(experience)}
                 >
-                  {experience.title}
-                </h3>
-                <div>
-                  <p
-                    className={`text-sm sm:text-base md:text-lg ${
+                  <h3
+                    className={`text-lg sm:text-xl md:text-2xl ${
                       selectedExperience === experience
-                        ? 'text-white'
+                        ? 'text-accent'
                         : 'text-gray-700'
                     }`}
                   >
-                    {experience.company}
-                  </p>
-                  <p
-                    className={`text-sm hidden sm:block sm:text-base md:text-lg ${
-                      selectedExperience === experience
-                        ? 'text-white'
-                        : 'text-gray-700'
-                    }`}
-                  >
-                    {experience.period}
-                  </p>
+                    {experience.title}
+                  </h3>
+                  <div>
+                    <p
+                      className={`text-sm sm:text-base md:text-lg ${
+                        selectedExperience === experience
+                          ? 'text-white'
+                          : 'text-gray-700'
+                      }`}
+                    >
+                      {experience.company}
+                    </p>
+                    <p
+                      className={`text-sm hidden sm:block sm:text-base md:text-lg ${
+                        selectedExperience === experience
+                          ? 'text-white'
+                          : 'text-gray-700'
+                      }`}
+                    >
+                      {experience.period}
+                    </p>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-          <div className="flex flex-col justify-center items-center w-full lg:w-2/5 hidden lg:block">
-            <div>
-              <h2 className="text-accent text-xl sm:text-2xl md:text-3xl lg:text-4xl">
-                {selectedExperience.title}
-              </h2>
-              <h3 className="text-white text-lg sm:text-xl md:text-2xl">
-                {selectedExperience.company}
-              </h3>
-            </div>
-            <ul className="text-white text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl list-disc pl-6 mt-4">
-              {selectedExperience.details.map((detail, index) => (
-                <li key={index}>{detail}</li>
               ))}
-            </ul>
+            </div>
+            <div className="flex flex-col justify-center items-center w-full lg:w-2/5 hidden lg:block">
+              <div>
+                <h2 className="text-accent text-xl sm:text-2xl md:text-3xl lg:text-4xl">
+                  {selectedExperience.title}
+                </h2>
+                <h3 className="text-white text-lg sm:text-xl md:text-2xl">
+                  {selectedExperience.company}
+                </h3>
+              </div>
+              <ul className="text-white text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl list-disc pl-6 mt-4">
+                {selectedExperience.details.map((detail, index) => (
+                  <li key={index}>{detail}</li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
---
+++ src/data/experienceData.js
@@ -0,0 +1,43 @@
+const experienceData = [
+  {
+    title: 'Software Development Specialist',
+    company: 'Ohio Department of Taxation',
+    period: "July '24 - Present",
+    details: [
+      'Tax stuff tax stuff tax stuff tax stuff tax stuff tax stuff',
+      'Tax stuff tax stuff tax stuff tax stuff tax stuff tax stuff',
+    ],
+  },
+  {
+    title: 'Software Engineer',
+    company: 'Linkt',
+    period: "Jan. '24 - June '24",
+    details: [
+      'Wrote some awesome code and did awesome things so awesomly',
+      'Wrote some awesome code and did awesome things so awesomly',
+      'Wrote some awesome code and did awesome things so awesomly',
+    ],
+  },
+  {
+    title: 'Co-Creator & Software Engineer',
+    company: 'Lambda Peeler (Open Source)',
+    period: "July '23 - Jan. '24",
+    details: [
+      'LAMBDA LAMBDA LAMBDA LAMBDA LAMBDA LAMBDA LAMBDA',
+      'LAMBDA  LAMBDA LAMBDA LAMBDA LAMBDA LAMBDA',
+      'LAMBDA LAMBDA LAMBDA LAMBDA LAMBDA LAMBDA LAMBDA',
+    ],
+  },
+  {
+    title: 'Java Teaching Assistant',
+    company: 'The Ohio State University',
+    period: "Aug. '22 - Dec. '22",
+    details: [
+      'Go bucks! Go bucks! Go bucks! Go bucks! Go bucks! Go bucks!',
+      'Go bucks! Go bucks! Go bucks! Go bucks! Go bucks! Go bucks!',
+      'Go bucks! Go bucks! Go bucks! Go bucks! Go bucks! Go bucks!',
+    ],
+  },
+];
+
+export default experienceData;
