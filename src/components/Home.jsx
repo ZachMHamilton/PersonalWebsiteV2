d3f0d50b10a89d07ae15a7b91eb8a87a38aef4fd
Zach Hamilton <zachmhamilton@outlook.com>
2024-06-11 21:08:34 -0400

    connected to vercel

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,38 +1,49 @@
 import { Link as ScrollLink } from 'react-scroll';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../utils/motion';
+import { useFadeInOnInView } from '../utils/useFadeInOnView';
 
 const Home = ({ setCurrent }) => {
+  const { ref, controls } = useFadeInOnInView(0.1);
   return (
-    <div
-      id="home"
-      className="flex flex-col items-center justify-center min-h-screen text-white"
+    <motion.div
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      variants={fadeIn('up', 'spring', 0, 2)}
     >
-      <div className="text-center">
-        <h1 className="text-5xl md:text-6xl font-bold">
-          Zach <span className="text-accent">Hamilton</span>
-        </h1>
-        <h2 className="text-2xl md:text-3xl mt-4">Software Engineer</h2>
-        <div className="flex gap-4 mt-8 justify-center">
-          <ScrollLink
-            to="experience"
-            smooth={true}
-            duration={500}
-            className="bg-primary hover:bg-accent text-white py-2 px-4 rounded cursor-pointer"
-            onClick={() => setCurrent('experience')}
-          >
-            Check Out My Work
-          </ScrollLink>
-          <ScrollLink
-            to="contact"
-            smooth={true}
-            duration={500}
-            className="bg-primary hidden md:block hover:bg-accent text-white py-2 px-4 rounded cursor-pointer"
-            onClick={() => setCurrent('contact')}
-          >
-            Get in Touch
-          </ScrollLink>
+      <div
+        id="home"
+        className="flex flex-col items-center justify-center min-h-screen text-white"
+      >
+        <div className="text-center">
+          <h1 className="text-5xl md:text-6xl font-bold">
+            Zach <span className="text-accent">Hamilton</span>
+          </h1>
+          <h2 className="text-2xl md:text-3xl mt-4">Software Engineer</h2>
+          <div className="flex gap-4 mt-8 justify-center">
+            <ScrollLink
+              to="experience"
+              smooth={true}
+              duration={500}
+              className="bg-primary hover:bg-accent text-white py-2 px-4 rounded cursor-pointer"
+              onClick={() => setCurrent('experience')}
+            >
+              Check Out My Work
+            </ScrollLink>
+            <ScrollLink
+              to="contact"
+              smooth={true}
+              duration={500}
+              className="bg-primary hidden md:block hover:bg-accent text-white py-2 px-4 rounded cursor-pointer"
+              onClick={() => setCurrent('contact')}
+            >
+              Get in Touch
+            </ScrollLink>
+          </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
