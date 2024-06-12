037b378bd11c1ae58131ff25f1497bfb4b68094b
Zach Hamilton <zachmhamilton@outlook.com>
2024-06-11 21:11:33 -0400

    moved images to public

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -5,7 +5,7 @@ const portfolioData = [
       'An open-source, web-based dashboard tailored for AWS Lambda developers. It is meticulously designed to bridge the gap between managing Lambda functions and layers, simplifying AWS cloud operations. It enhances developing efficiency by allowing engineers to test their layer and function compatibilities in bulk. Currently, engineers can only test one-by-one or set up a very robust CI/CD pipeline through AWS. Our product aims to bridge the gap and give engineers an easy-to-use alternative.',
     link: 'https://lambda-peeler.onrender.com/',
     linkText: 'Website',
-    imgSrc: '../src/assets/connecting.gif',
+    imgSrc: '../public/connecting.gif',
   },
   {
     title: 'Shelved',
@@ -13,7 +13,7 @@ const portfolioData = [
       'A digital library app for avid readers, audiobook listeners, and e-book enjoyers. It can be hard to keep track of all the books you have read/listened to across multiple platforms. Shelved allows you to maintain a library of ALL of the books you have enjoyed. It is seamlessly integrated with the GoogleBooks API that allows users to view more information about each book, such as author, genre, and number of pages.',
     link: 'https://github.com/ZachMHamilton/Library-Project',
     linkText: 'Docs',
-    imgSrc: '../src/assets/library.png',
+    imgSrc: '../public/library.png',
   },
   {
     title: 'Algo Archive',
@@ -21,7 +21,7 @@ const portfolioData = [
       'A web-based dashboard that allows users to track their algorithm practice in preparation for technical interviews. Users can save their algo problems with the question and their solution, as well as any notes they would like to save, such as difficulty, different strategies used, and more.',
     link: 'https://github.com/danger-noodle-42/algo-archive',
     linkText: 'Docs',
-    imgSrc: '../src/assets/algo.png',
+    imgSrc: '../public/algo.png',
   },
 ];
 
